bd80315135f7e176fa1640fbadbb864ec59f70aa
George Ornbo <george@shapeshed.com>
2023-03-07 11:19:14 +0000

    Update to use pdfcrop, lint and fix

--- remarkable-crossword.sh
+++ remarkable-crossword.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
 set -o pipefail
-DATE=`date +%Y%m%d`
+DATE=$(date +%Y%m%d)
 URL="https://crosswords-static.guim.co.uk"
 QUICK="gdn.quick.$DATE.pdf"
+CROPQUICK="gdn.quick.$DATE-crop.pdf"
 CRYPTIC="gdn.cryptic.$DATE.pdf"
+CROPCRYPTIC="gdn.cryptic.$DATE-crop.pdf"
 
 # Send to Remarkable
 
 # Quick crosswords published Mon-Sat
-if [ $(date +%u) -le 6 ]
-then
-    p2r -p crosswords $URL/$QUICK
+if [ "$(date +%u)" -le 6 ]; then
+    wget -q -P /tmp "$URL"/"$QUICK"
+    pdfcrop --margins 10 /tmp/"$QUICK"
+    rmapi put /tmp/"$CROPQUICK" /crosswords
 fi
 # Cryptic crosswords published Mon-Fri
-if [ $(date +%u) -le 5 ]
-then
-    p2r -p crosswords $URL/$CRYPTIC
+if [ "$(date +%u)" -le 5 ]; then
+    wget -q -P /tmp "$URL"/"$CRYPTIC"
+    pdfcrop --margins 10 /tmp/"$CRYPTIC"
+    rmapi put /tmp/"$CROPCRYPTIC" /crosswords
 fi
-
-
